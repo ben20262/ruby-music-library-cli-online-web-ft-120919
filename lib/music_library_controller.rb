ab87b13e52844d3cfa34efb5e42568a46a69f443
Benjamin Small <ben20262@aim.com>
2020-01-09 18:25:16 +0000

    Automatically backed up by Learn

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,53 @@
+class MusicLibraryController
+  attr_reader :path
+
+  def initialize(path = "./db/mp3s")
+    @path = path
+    music = MusicImporter.new(@path)
+    music.import
+  end
+
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+
+    input = gets.strip
+    if input == "exit"
+      return
+    else
+      call
+    end
+  end
+
+  def list_songs
+    list = Song.all.sort_by {|song| song.name}
+    count = 1
+    list.each do |song|
+      words = [song.artist.name, song.name, song.genre.name].join(" - ")
+      puts "#{count}. #{words}"
+      count += 1
+    end
+  end
+
+  def list_artists
+  end
+
+  def list_genres
+  end
+
+  def list_songs_by_artist
+  end
+
+  def list_songs_by_genre
+  end
+
+  def play_song
+  end
+end
